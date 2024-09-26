03688db3e1d12d5ca35953a13d25ee8787f72854
Dhruvsinghdk <re621006@gmail.com>
2024-09-26 13:19:53 +0530

    new

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,30 @@
-// inheritance
-
-class A{
-    protected int i = 12;
-    private int j = 24;
-
-    int setj(){
-        return j;
-    }
-    void show(){
-        System.out.println("class A");
-    }
-}
-
-class B extends A {
-    int a = 10;
-
-    void display(){
-        System.out.println(setj());
-        System.out.println(i);
-    }
-
-}
-public class Main {
-    public static void main(String[] args) {
-
-        B n2 = new B();
-        n2.show();
-        n2.display();
-
-    }
-}
+//// inheritance
+//
+//class A{
+//    protected int i = 12;
+//    private int j = 24;
+//
+//    int setj(){
+//        return j;
+//    }
+//    void show(){
+//        System.out.println("class A");
+//    }
+//}
+//class B extends A {
+//    int a = 10;
+//
+//    void display(){
+//        System.out.println(setj());
+//        System.out.println(i);
+//    }
+//}
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        B n2 = new B();
+//        n2.show();
+//        n2.display();
+//
+//    }
+//}
